fa227fc74be6c1548f5c07a8e0698cf9ddd6746e
Asta Myllymaki <myllymaki.asta@gmail.com>
2018-02-22 14:43:12 +0000

    change mlab as a database

--- app.js
+++ app.js
@@ -16,7 +16,8 @@ var commentRoutes = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes = require("./routes/index");
 
-mongoose.connect("mongodb://localhost/yelp_camp");
+//mongoose.connect("mongodb://localhost/yelp_camp");
+mongoose.connect("mongodb://asta:ASTAkoodaa@ds145438.mlab.com:45438/astan_yelpcamp");
 app.set("view engine", "ejs");
 app.use(express.static(__dirname +"/public"));
 app.use(bodyParser.urlencoded({extended: true}));
